0521fb08414f2ae6130cd15a1bab704643bd319f
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-08-24 00:04:14 +0300

    initial commit

---
+++ src/works/Magic_mushrooms.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
+function MagicMushrooms() {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <div className='mushroom-top w-36 h-36 rounded-full bg-red-500 relative top-20'>
+
+      </div>
+      <div className='mushroom-bottom w-12 h-20 bg-gray-100'>
+
+      </div>
+    </div>
+  );
+}
+
+export default MagicMushrooms;
